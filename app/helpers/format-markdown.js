8d368e32b3e9f54b7563beab937ff9db13393890
Devon Bleibtrey <bleib1dj@gmail.com>
2015-04-21 20:36:33 -0400

    Working iteration

--- app/helpers/format-markdown.js
+++
@@ -1,5 +0,0 @@
-import Ember from "ember";
-var showdown = new Showdown.converter();
-export default Ember.Handlebars.makeBoundHelper(function(value) {
-  return new Ember.Handlebars.SafeString(showdown.makeHtml(value));
-});
